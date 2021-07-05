957e3e19114525a2c954b9944359fbd77bdb5c61
Willian da Silva <wssilva.willian@gmail.com>
2021-07-05 11:28:33 -0300

    Adicionando o script de execucao do build

---
+++ devops/scripts/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+check_sucessful(){
+    if [ $? != 0 ];
+    then
+        echo "Error Execution"
+        exit 1
+    fi
+}
+
+login() {
+    aws ecr get-login-password --region ${AWS_REGION} | \
+        docker login --username AWS --password-stdin ${AWS_ACCOUNT_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com
+}
+
+build_push() {
+    docker build -t ${PROJECT_NAME}:${VERSION} -f devops/application/build/Dockerfile .
+        check_sucessful
+
+    docker tag ${PROJECT_NAME}:${VERSION} ${AWS_ACCOUNT_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/${PROJECT_NAME}:${VERSION}
+        check_sucessful
+
+    docker push ${AWS_ACCOUNT_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/${PROJECT_NAME}:${VERSION}
+        check_sucessful
+
+    docker rmi ${AWS_ACCOUNT_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com/${PROJECT_NAME}:${VERSION}
+        check_sucessful
+
+    docker rmi ${PROJECT_NAME}:${VERSION}
+        check_sucessful
+}
+
+AWS_ACCOUNT_REGISTRY=$(aws sts get-caller-identity --output text |awk '{print $1}')
+    check_sucessful
+
+VERSION="${GITHUB_RUN_ID}"
+
+login
+    check_sucessful
+
+build_push
+    check_sucessful
