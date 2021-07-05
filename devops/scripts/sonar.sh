bd939710e31aa73d103ffe46ba3d5b5d2bf46d04
Willian da Silva <wssilva.willian@gmail.com>
2021-07-05 11:28:04 -0300

    Adicionando o script de execucao do sonar

---
+++ devops/scripts/sonar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+check_sucessful(){
+    if [ $? != 0 ];
+    then
+        echo "Error Execution"
+        exit 1
+    fi
+}
+
+sonar_build() {
+    docker build -t ${PROJECT_NAME}-sonar:${VERSION} -f devops/application/sonar/Dockerfile .
+}
+
+sonar_execute() {
+
+    docker run \
+        --rm \
+        --mount source=${VOlUME_NAME},target=/app \
+        -e VERSION="${VERSION}" \
+        -e PROJECT_NAME="${PROJECT_NAME}" \
+        -e SONAR_HOST="${SONAR_HOST}" \
+        -e SONAR_TOKEN="${SONAR_TOKEN}" \
+        -e SONAR_EXCLUSIONS="**/Migrations/**" \
+        -e SONAR_COVERAGE_EXCLUSIONS="**/Models/**,**/Migrations/**,**/Enums/**,**/Migrations/**,**/DbContexts/**,**/Entities/**,**/devops/**,**/Program.cs,**/Startup.cs" \
+        ${PROJECT_NAME}-sonar:${VERSION}
+
+    docker rmi ${PROJECT_NAME}-sonar:${VERSION}
+        check_sucessful
+    
+    docker volume rm ${VOlUME_NAME}
+        check_sucessful
+}
+
+VERSION="${GITHUB_RUN_ID}"
+VOlUME_NAME="${PROJECT_NAME}-volume-${VERSION}"
+
+sonar_build
+    check_sucessful
+
+sonar_execute
+    check_sucessful
