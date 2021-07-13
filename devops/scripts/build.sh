aef3e06e429de0b488c76d21342790ae633c7330
Willian da Silva <wssilva.willian@gmail.com>
2021-07-12 23:24:19 -0300

    Corrigindo o profile

--- devops/scripts/build.sh
+++ devops/scripts/build.sh
@@ -7,6 +7,7 @@ check_sucessful(){
     fi
 }
 
+
 login() {
     aws ecr get-login-password --region ${AWS_REGION} | \
         docker login --username AWS --password-stdin ${AWS_ACCOUNT_REGISTRY}.dkr.ecr.${AWS_REGION}.amazonaws.com
